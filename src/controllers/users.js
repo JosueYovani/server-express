8ac22b696aa0d745820938952532834d607eac73
Josué Yovani <josue.yovani@outlook.com>
2023-10-18 12:43:10 -0600

    Add controllers for user create and auth

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,48 @@
 /** Imports **/
 const Users = require("../models/users");
 
-const createUser = async (userData) => {};
+/** Controller create new user  **/
+const createUser = async (req, res) => {
+  try {
+    const newUser = await Users.create(req.body);
+    newUser.password = await Users.encrypPassword(newUser.password);
+    if (!newUser) {
+      return res.status(502).send({ msg: "User not created", err: newUser });
+    }
+    await newUser.save();
+    return res
+      .status(201)
+      .json({ msg: "User created!", data: { data: newUser } });
+  } catch (error) {
+    return res
+      .status(error.status || 500)
+      .json({ msg: error.message || "unknown" });
+  }
+};
+
+/** Controller authentication **/
+const authUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Users.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+    let validPass = await Users.comparePassword(password, user.password);
+    if (!validPass) {
+      return res.status(401).json({ msg: "Incorrect password" });
+    }
+    // Generate token
+    let token = auth.generateToken(user);
+    return res.status(200).json({ msg: "Success", data: token });
+  } catch (error) {
+    return res
+      .status(error.status || 500)
+      .json({ msg: error.message || "unknown" });
+  }
+};
+
+module.exports = {
+  createUser,
+  authUser,
+};
