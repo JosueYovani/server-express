c583ebce6bd0ddd69997be47e00c92fd42c84b01
Josué Yovani <josue.yovani@outlook.com>
2023-10-18 13:30:46 -0600

    Refactor controllers users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -14,33 +14,10 @@ const createUser = async (req, res) => {
   } catch (error) {
     return res
       .status(error.status || 500)
-      .json({ msg: error.message || "unknown" });
-  }
-};
-
-/** Controller authentication **/
-const authUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await Users.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ msg: "User not found" });
-    }
-    let validPass = await Users.comparePassword(password, user.password);
-    if (!validPass) {
-      return res.status(401).json({ msg: "Incorrect password" });
-    }
-    // Generate token
-    let token = auth.generateToken(user);
-    return res.status(200).json({ msg: "Success", data: token });
-  } catch (error) {
-    return res
-      .status(error.status || 500)
-      .json({ msg: error.message || "unknown" });
+      .json({ msg: error.message || "Unknown" });
   }
 };
 
 module.exports = {
   createUser,
-  authUser,
 };
