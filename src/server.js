d38102fcdf0235858dca4945b09eb90f75ce1655
Josué Yovani <josue.yovani@outlook.com>
2023-10-19 10:44:48 -0600

    Add lib cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 /** Imports **/
 const apiRoutes = require("./routes");
@@ -9,6 +10,7 @@ const app = express();
 /** Middlewares **/
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
 
 // Routes
 app.get("/", (req, res) => {
